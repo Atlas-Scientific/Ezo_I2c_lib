65f6f7d5b0b95d343f0de7d396e9e025352f4c75
parma <pshiwmangal@atlas-scientific.com>
2021-03-15 18:03:10 -0400

    Added code for large dosing pump.

--- Examples/IOT_kits/pool_kit/pool_kit.ino
+++ Examples/IOT_kits/pool_kit/pool_kit.ino
@@ -1,8 +1,8 @@
 #include <iot_cmd.h>
 #include <ESP8266WiFi.h>                                         //include esp8266 wifi library 
 #include "ThingSpeak.h"                                          //include thingspeak library
-#include <sequencer4.h>                                          //imports a 4 function sequencer
-#include <sequencer1.h>                                          //imports a 1 function sequencer
+#include <sequencer4.h>                                          //imports a 4 function sequencer 
+#include <sequencer1.h>                                          //imports a 1 function sequencer 
 #include <Ezo_i2c_util.h>                                        //brings in common print statements
 #include <Ezo_i2c.h> //include the EZO I2C library from https://github.com/Atlas-Scientific/Ezo_I2c_lib
 #include <Wire.h>    //include arduinos i2c library
@@ -17,14 +17,16 @@ const char * myWriteAPIKey = "XXXXXXXXXXXXXXXX";                 //Your ThingSpe
 //------------------------------------------------------------------
 
 
-Ezo_board PH = Ezo_board(99, "PH");       //create a PH circuit object, who's address is 99 and name is "PH"
-Ezo_board ORP = Ezo_board(98, "ORP");     //create an ORP circuit object who's address is 98 and name is "ORP"
-Ezo_board RTD = Ezo_board(102, "RTD");    //create an RTD circuit object who's address is 102 and name is "RTD"
+Ezo_board PH = Ezo_board(99, "PH");           //create a PH circuit object, who's address is 99 and name is "PH"
+Ezo_board ORP = Ezo_board(98, "ORP");         //create an ORP circuit object who's address is 98 and name is "ORP"
+Ezo_board RTD = Ezo_board(102, "RTD");        //create an RTD circuit object who's address is 102 and name is "RTD"
+Ezo_board PMPL = Ezo_board(109, "PMPL");      //create an PMPL circuit object who's address is 109 and name is "PMPL"
 
-Ezo_board device_list[] = {               //an array of boards used for sending commands to all or specific boards
+Ezo_board device_list[] = {   //an array of boards used for sending commands to all or specific boards
   PH,
   ORP,
   RTD,
+  PMPL
 };
 
 Ezo_board* default_board = &device_list[0]; //used to store the board were talking to
@@ -34,64 +36,72 @@ const uint8_t device_list_len = sizeof(device_list) / sizeof(device_list[0]);
 
 //enable pins for each circuit
 const int EN_PH = 14;
-const int EN_ORP  = 12;
+const int EN_ORP = 12;
 const int EN_RTD = 15;
 const int EN_AUX = 13;
 
-const unsigned long reading_delay = 1000;                 //how long we wait to receive a response, in milliseconds 
+const unsigned long reading_delay = 1000;                 //how long we wait to receive a response, in milliseconds
 const unsigned long thingspeak_delay = 15000;             //how long we wait to send values to thingspeak, in milliseconds
 
-unsigned int poll_delay = thingspeak_delay - reading_delay * 2 - 300;  //how long to wait between polls after accounting for the times it takes to send readings
+unsigned int poll_delay = 2000 - reading_delay * 2 - 300; //how long to wait between polls after accounting for the times it takes to send readings
+
+//parameters for setting the pump output
+#define PUMP_BOARD        PMPL      //the pump that will do the output (if theres more than one)
+#define PUMP_DOSE         10        //the dose that the pump will dispense in  milliliters
+#define EZO_BOARD         PH        //the circuit that will be the target of comparison
+#define IS_GREATER_THAN   true      //true means the circuit's reading has to be greater than the comparison value, false mean it has to be less than
+#define COMPARISON_VALUE  7         //the threshold above or below which the pump is activated
 
 float k_val = 0;                                          //holds the k value for determining what to print in the help menu
 
 bool polling  = true;                                     //variable to determine whether or not were polling the circuits
 bool send_to_thingspeak = true;                           //variable to determine whether or not were sending data to thingspeak
 
-bool wifi_isconnected(){                            //function to check if wifi is connected
+bool wifi_isconnected() {                           //function to check if wifi is connected
   return (WiFi.status() == WL_CONNECTED);
 }
 
-void reconnect_wifi(){                                    //function to reconnect wifi if its not connected
-  if(!wifi_isconnected()){
+void reconnect_wifi() {                                   //function to reconnect wifi if its not connected
+  if (!wifi_isconnected()) {
     WiFi.begin(ssid, pass);
     Serial.println("connecting to wifi");
   }
 }
 
-void thingspeak_send(){
+void thingspeak_send() {
   if (send_to_thingspeak == true) {                                                    //if we're datalogging
-    if(wifi_isconnected()){
-      int return_code = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey); 
+    if (wifi_isconnected()) {
+      int return_code = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
       if (return_code == 200) {                                                          //code for successful transmission
-          Serial.println("sent to thingspeak");
-      }else{
+        Serial.println("sent to thingspeak");
+      } else {
         Serial.println("couldnt send to thingspeak");
       }
     }
   }
 }
 
-void step1();    //forward declarations of functions to use them in the sequencer before defining them
+void step1();      //forward declarations of functions to use them in the sequencer before defining them
 void step2();
 void step3();
 void step4();
-
-Sequencer4 Seq(&step1, reading_delay,         //calls the steps in sequence with time in between them
-               &step2, 300, 
-               &step3, reading_delay, 
+Sequencer4 Seq(&step1, reading_delay,   //calls the steps in sequence with time in between them
+               &step2, 300,
+               &step3, reading_delay,
                &step4, poll_delay);
 
-Sequencer1 Wifi_Seq (&reconnect_wifi, 10000); //calls the wifi reconnect function every 10 seconds
+Sequencer1 Wifi_Seq(&reconnect_wifi, 10000);  //calls the wifi reconnect function every 10 seconds
+
+Sequencer1 Thingspeak_seq(&thingspeak_send, thingspeak_delay); //sends data to thingspeak with the time determined by thingspeak delay
 
 void setup() {
 
   pinMode(EN_PH, OUTPUT);                                                         //set enable pins as outputs
-  pinMode(EN_ORP , OUTPUT);
+  pinMode(EN_ORP, OUTPUT);
   pinMode(EN_RTD, OUTPUT);
   pinMode(EN_AUX, OUTPUT);
   digitalWrite(EN_PH, LOW);                                                       //set enable pins to enable the circuits
-  digitalWrite(EN_ORP , LOW);
+  digitalWrite(EN_ORP, LOW);
   digitalWrite(EN_RTD, HIGH);
   digitalWrite(EN_AUX, LOW);
 
@@ -102,28 +112,57 @@ void setup() {
   ThingSpeak.begin(client);               //enable ThingSpeak connection
   Wifi_Seq.reset();                       //initialize the sequencers
   Seq.reset();
+  Thingspeak_seq.reset();
 }
 
 void loop() {
- String cmd;                            //variable to hold commands we send to the kit
+  String cmd;                            //variable to hold commands we send to the kit
 
   Wifi_Seq.run();                        //run the sequncer to do the polling
-  
+
   if (receive_command(cmd)) {            //if we sent the kit a command it gets put into the cmd variable
-    polling = false;                     //we stop polling  
+    polling = false;                     //we stop polling
     send_to_thingspeak = false;          //and sending data to thingspeak
-    if(!process_coms(cmd)){              //then we evaluate the cmd for kit specific commands
+    if (!process_coms(cmd)) {            //then we evaluate the cmd for kit specific commands
       process_command(cmd, device_list, device_list_len, default_board);    //then if its not kit specific, pass the cmd to the IOT command processing function
     }
   }
-  
+
   if (polling == true) {                 //if polling is turned on, run the sequencer
     Seq.run();
+    Thingspeak_seq.run();
   }
 }
 
-void step1() { 
-  //send a read command. we use this command instead of RTD.send_cmd("R"); 
+//function that controls the pumps activation and output
+void pump_function(Ezo_board &pump, Ezo_board &sensor, float value, float dose, bool greater_than) {
+  if (sensor.get_error() == Ezo_board::SUCCESS) {                    //make sure we have a valid reading before we make any decisions
+    bool comparison = false;                                        //variable for holding the reuslt of the comparison
+    if (greater_than) {                                             //we do different comparisons depending on what the user wants
+      comparison = (sensor.get_last_received_reading() >= value);   //compare the reading of the circuit to the comparison value to determine whether we actiavte the pump
+    } else {
+      comparison = (sensor.get_last_received_reading() <= value);
+    }
+    if (comparison) {                                               //if the result of the comparison means we should activate the pump
+      pump.send_cmd_with_num("d,", dose);                           //dispense the dose
+      delay(100);                                                   //wait a few milliseconds before getting pump results
+      Serial.print(pump.get_name());                                //get pump data to tell the user if the command was received successfully
+      Serial.print(" ");
+      char response[20];
+      if (pump.receive_cmd(response, 20) == Ezo_board::SUCCESS) {
+        Serial.print("pump dispensed ");
+      } else {
+        Serial.print("pump error ");
+      }
+      Serial.println(response);
+    } else {
+      pump.send_cmd("x");                                          //if we're not supposed to dispense, stop the pump
+    }
+  }
+}
+
+void step1() {
+  //send a read command. we use this command instead of RTD.send_cmd("R");
   //to let the library know to parse the reading
   RTD.send_read_cmd();
 }
@@ -135,7 +174,7 @@ void step2() {
     PH.send_cmd_with_num("T,", RTD.get_last_received_reading());
     ThingSpeak.setField(3, String(RTD.get_last_received_reading(), 2));                 //assign temperature readings to the third column of thingspeak channel
   } else {                                                                                      //if the temperature reading is invalid
-    PH.send_cmd_with_num("T,", 25.0);
+    PH.send_cmd_with_num("T,", 25.0);                                                           //send default temp = 25 deg C to PH sensor
     ThingSpeak.setField(3, String(25.0, 2));                 //assign temperature readings to the third column of thingspeak channel
   }
 
@@ -152,46 +191,42 @@ void step3() {
 void step4() {
   receive_and_print_reading(PH);             //get the reading from the PH circuit
   if (PH.get_error() == Ezo_board::SUCCESS) {                                          //if the PH reading was successful (back in step 1)
-     ThingSpeak.setField(1, String(PH.get_last_received_reading(), 2));                 //assign PH readings to the first column of thingspeak channel
+    ThingSpeak.setField(1, String(PH.get_last_received_reading(), 2));                 //assign PH readings to the first column of thingspeak channel
   }
   Serial.print("  ");
-  receive_and_print_reading(ORP);             //get the reading from the EC circuit
-  if (ORP.get_error() == Ezo_board::SUCCESS) {                                          //if the EC reading was successful (back in step 1)
-     ThingSpeak.setField(2, String(ORP.get_last_received_reading(), 0));                 //assign EC readings to the second column of thingspeak channel
+  receive_and_print_reading(ORP);             //get the reading from the ORP circuit
+  if (ORP.get_error() == Ezo_board::SUCCESS) {                                          //if the ORP reading was successful (back in step 1)
+    ThingSpeak.setField(2, String(ORP.get_last_received_reading(), 0));                 //assign ORP readings to the second column of thingspeak channel
   }
-
   Serial.println();
-
-  thingspeak_send();                                                //send circuit data to thingspeak
+  pump_function(PUMP_BOARD, EZO_BOARD, COMPARISON_VALUE, PUMP_DOSE, IS_GREATER_THAN);
 }
 
 void start_datalogging() {
   polling = true;                                                 //set poll to true to start the polling loop
   send_to_thingspeak = true;
-  poll_delay = thingspeak_delay - reading_delay * 2 - 300;        //set poll delay to take reading times into account
-  Seq.reset();
-  
+  Thingspeak_seq.reset();
 }
 
-bool process_coms(const String &string_buffer) {                   //function to process commands that manipulate global variables and are specifc to certain kits
+bool process_coms(const String &string_buffer) {      //function to process commands that manipulate global variables and are specifc to certain kits
   if (string_buffer == "HELP") {
     print_help();
     return true;
   }
   else if (string_buffer.startsWith("DATALOG")) {
-     start_datalogging();
+    start_datalogging();
     return true;
   }
   else if (string_buffer.startsWith("POLL")) {
-    polling = true;  
+    polling = true;
     Seq.reset();
-    
+
     int16_t index = string_buffer.indexOf(',');                    //check if were passing a polling delay parameter
     if (index != -1) {                                              //if there is a polling delay
       float new_delay = string_buffer.substring(index + 1).toFloat(); //turn it into a float
 
-      float mintime = reading_delay*2 + 300;
-      if (new_delay >= (mintime/1000.0)) {                                       //make sure its greater than our minimum time
+      float mintime = reading_delay * 2 + 300;
+      if (new_delay >= (mintime / 1000.0)) {                                     //make sure its greater than our minimum time
         Seq.set_step4_time((new_delay * 1000.0) - mintime);          //convert to milliseconds and remove the reading delay from our wait
       } else {
         Serial.println("delay too short");                          //print an error if the polling time isnt valid
@@ -199,7 +234,7 @@ bool process_coms(const String &string_buffer) {                   //function to
     }
     return true;
   }
-  return false;    //return false if the command is not in the list, so we can scan the other list or pass it to the circuit
+  return false;                         //return false if the command is not in the list, so we can scan the other list or pass it to the circuit
 }
 
 void print_help() {
@@ -215,7 +250,7 @@ void print_help() {
   Serial.println(F("ph:cal,clear     clear calibration                                         "));
   Serial.println(F("                                                                           "));
   Serial.println(F("orp:cal,225          calibrate orp probe to 225mV                          "));
-  Serial.println(F("orp:cal,clear        clear calibration                                     ")); 
+  Serial.println(F("orp:cal,clear        clear calibration                                     "));
   Serial.println(F("                                                                           "));
   Serial.println(F("rtd:cal,t            calibrate the temp probe to any temp value            "));
   Serial.println(F("                     t= the temperature you have chosen                    "));
