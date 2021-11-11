b524b02f21ba8d5552e5f2d59d11b1361c4e433b
parma <pshiwmangal@atlas-scientific.com>
2021-11-11 17:18:21 -0500

    modified code to clear readings after each iteration

--- Examples/Projects/arduino_co2_meter/arduino_co2_meter.ino
+++ Examples/Projects/arduino_co2_meter/arduino_co2_meter.ino
@@ -23,11 +23,14 @@ void setup() {
   Seq.reset();                                //initialize the sequencer
   CO2_lcd.begin(16, 2);                       //initialize the 16x2 lcd
   CO2_lcd.setBacklight(HIGH);                 //turn on backlight of lcd
+  CO2_lcd.setCursor(1, 0);                    //set cursor to column 1, row 0
+  CO2_lcd.print("Carbon Dioxide");            //sends the words "Carbon Dioxide" to line 1 of the display
+  CO2_lcd.setCursor(10, 1);                   //set cursor to column 10, row 1
+  CO2_lcd.print("ppm");                       //send the words "ppm" to the display
 }
 
 void loop() {
   Seq.run();                                  //run the sequncer to get the co2 readings
-  lcd_print();                                //send the Co2 reading to the LCD
 }
 
 void step1() {
@@ -37,13 +40,13 @@ void step1() {
 void step2() {
   receive_and_print_reading(CO2);             //get the reading from the CO2 sensor
   Serial.println();                           //and send it out through the serial monitor (this is for debugging, you do not need to do this)
+  lcd_print();                                //send the Co2 reading to the LCD
 }
 
 void lcd_print() {                                    //prints the co2 reading to the LCD
-  CO2_lcd.setCursor(1, 0);                            //set cursor to column 1, row 0
-  CO2_lcd.print("Carbon Dioxide");                    //sends the words "Carbon Dioxide" to line 1 of the display
-  CO2_lcd.setCursor(3, 1);                            //moves the cursor to column 2, row 1
+
+  CO2_lcd.setCursor(3, 1);                            //moves the cursor to column 3, row 1
+  CO2_lcd.print("     ");                             //clears the CO2 readings on the display after each iteration 
+  CO2_lcd.setCursor(3, 1);                            //moves the cursor to column 3, row 1
   CO2_lcd.print(CO2.get_last_received_reading(), 0);  //sends the CO2 reading to the display
-  CO2_lcd.setCursor(10, 1);                           //set cursor to column 10, row 1
-  CO2_lcd.print("ppm");                               //send the words "ppm" to the display
 }
